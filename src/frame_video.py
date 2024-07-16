06a7401670f41ed38c027250f290e58d1d0d67b5
gakucode0602 <dangcapcothua2903@gmail.com>
2024-07-16 16:31:00 +0700

    updating

--- src/frame_video.py
+++ src/frame_video.py
@@ -1,39 +1,66 @@
 import cv2
 import os
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image
+import torch
+import time
+from tqdm.notebook import tqdm
+from facenet_pytorch import MTCNN
 
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 face_cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-path_video_face = 'dataset\\raw_video'
-path_frame_face = 'dataset\\processed'
+path_video_face = 'Project\\SIC_Project\\Data\\Dataset' # đường dẫn tới folder video
+path_frame = 'Project\\SIC_Project\\Data\\NewData' # đường dẫn tới folder ảnh cắt từ video
+path_frame_face = 'Project\\SIC_Project\\Data\\NewData' # đường dẫn tới ảnh sau khi cắt từ frame
 
+
+# Source code cũ
+# def extract_faces_from_video(video_path, output_dir, face_cascade_path):
+#     print(video_path)
+#     if not os.path.exists(output_dir):
+#         os.makedirs(output_dir)
+    
+#     cap = cv2.VideoCapture(video_path)
+#     face_cascade = cv2.CascadeClassifier(face_cascade_path)
+#     frame_count = 0
+
+#     while cap.isOpened():
+        
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+        
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+#         count = 0
+#         for (x, y, w, h) in faces:
+#             if count == 50:
+#                 break
+#             face = frame[y:y+h, x:x+w]
+#             face_filename = os.path.join(output_dir, f"face_{frame_count}.jpg")
+#             cv2.imwrite(face_filename, face)
+#             frame_count += 1
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+#     print(video_path)
+
+# Code mới
 def extract_faces_from_video(video_path, output_dir, face_cascade_path):
     print(video_path)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    
     cap = cv2.VideoCapture(video_path)
-    face_cascade = cv2.CascadeClassifier(face_cascade_path)
-    frame_count = 0
-
-    while cap.isOpened():
-        
-        ret, frame = cap.read()
-        if not ret:
+    for i in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))):
+        success, image = cap.read()
+        if not success:
             break
+        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
         
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-        for (x, y, w, h) in faces:
-            face = frame[y:y+h, x:x+w]
-            face_filename = os.path.join(output_dir, f"face_{frame_count}.jpg")
-            cv2.imwrite(face_filename, face)
-            frame_count += 1
-
-    cap.release()
-    cv2.destroyAllWindows()
-    print(video_path)
-
+    pass
 
 if __name__ == '__main__':
     map_label = {}
