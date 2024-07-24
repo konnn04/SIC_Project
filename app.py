109cd3ba0a8b21aa643c1b9c6ba5205c8aff48c3
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-07-24 19:01:34 +0700

    update

--- app.py
+++ app.py
@@ -1,57 +1,55 @@
-from flask import Flask, render_template, Response, request, jsonify
+from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
 import cv2
 import os
-# import face_recognition
-import numpy as np
+import tensorflow.compat.v1 as tf
+import random
+import src.indentify as identify
+import imutils
+import time
 
-app = Flask(__name__)
+FRAME = 15
 
 # Load your model here
-# model = load_model()
+identify.start()
 
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template("diemdanh.html")
+app = Flask(__name__)
+socketio = SocketIO(app)
 
-def gen():
-    """Video streaming generator function."""
-    cap = cv2.VideoCapture(0)
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            # Convert the image from BGR color (which OpenCV uses) to RGB color
-            rgb_frame = frame[:, :, ::-1]
+with app.app_context():  
+    @app.route('/')
+    def index():
+        socketio.start_background_task(target=gen)
+        return render_template("diemdanh.html")
 
-            # Now you can use face_recognition library to recognize faces
-            # For example, if you have a loaded model to predict
-            # face_locations = face_recognition.face_locations(rgb_frame)
-            # face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-            # predictions = model.predict(face_encodings)
+    def gen():    
+        cap = cv2.VideoCapture(0)              
+        while True:
+            
+            success, frame = cap.read()
+            if not success:
+                break
+            else:           
+                frame = cv2.flip(frame, 1)               
+                frame = imutils.resize(frame, width=600)
 
-            # Just for demonstration, we will convert the frame to JPEG and return it
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                best_name, best_class_probabilities, frame = identify.identify(frame)
+                socketio.emit('update_results', {'name': best_name, 'accuracy': best_class_probabilities})
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            # time.sleep(round(1/FRAME, 2))
+                    
 
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    @app.route('/video_feed')
+    def video_feed():    
+        return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    # Here you would process the image sent from the form to recognize the face
-    # For simplicity, we'll just return a dummy response
-    data = request.json
-    # Process data['image'] with your face recognition model
-    # For now, we'll just pretend we recognized a face with 99% accuracy
-    response = {'label': 'John Doe', 'accuracy': 99}
-    return jsonify(response)
+    @socketio.on('connect')
+    def handle_connect():
+        print("Client connected") 
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    if __name__ == '__main__':
+        # app.run(debug=True, threaded=True)
+        socketio.run(app, debug=True)
---
+++ src/indentify.py
@@ -0,0 +1,117 @@
+import cv2
+import os
+import numpy as np
+import pickle
+import src.facenet as facenet
+import src.align.detect_face as align
+import collections
+import tensorflow.compat.v1 as tf
+import imutils
+import time
+
+# model, class_names,images_placeholder, embeddings, sess = None, None, None, None, None
+
+CLASSIFIER_PATH = os.path.join(os.getcwd(), 'models/classifier.pkl')
+FACENET_MODEL_PATH = os.path.join(os.getcwd(), 'models/20180402-114759.pb')
+MINSIZE = 20
+THRESHOLD = [0.6, 0.7, 0.7]
+FACTOR = 0.709
+IMAGE_SIZE = 182
+INPUT_IMAGE_SIZE = 160
+
+person_detected = collections.Counter()
+# Load your model here
+def load_model():
+    # Load The Custom Classifier
+    with open(CLASSIFIER_PATH, 'rb') as file:
+        model, class_names = pickle.load(file)
+    print("Custom Classifier, Successfully loaded")
+    return model, class_names
+
+def load_facenet():
+    with tf.Graph().as_default():
+        # Cai dat GPU neu co
+        gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.6)
+        sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+
+        with sess.as_default():
+            # Load the model
+            print('Loading feature extraction model')
+            facenet.load_model(FACENET_MODEL_PATH)
+
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            pnet, rnet, onet = align.create_mtcnn(sess, "src/align")
+            person_detected = collections.Counter()
+    return images_placeholder, phase_train_placeholder, embeddings, sess, pnet, rnet, onet
+
+def identify_face(frame, model, class_names, images_placeholder, phase_train_placeholder, embeddings, sess, pnet, rnet, onet):
+    bounding_boxes, _ = align.detect_face(frame, MINSIZE, pnet, rnet, onet, THRESHOLD, FACTOR)
+    faces_found = bounding_boxes.shape[0]
+    try:
+        # if faces_found > 1:
+        #     cv2.putText(frame, "Only one face", (0, 100), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+        #                 1, (255, 255, 255), thickness=1, lineType=2)
+        # elif faces_found > 0:
+        if faces_found > 0:
+            det = bounding_boxes[:, 0:4]
+            bb = np.zeros((faces_found, 4), dtype=np.int32)
+            for i in range(faces_found):
+                bb[i][0] = det[i][0]
+                bb[i][1] = det[i][1]
+                bb[i][2] = det[i][2]
+                bb[i][3] = det[i][3]
+                # print(bb[i][3]-bb[i][1])
+                # print(frame.shape[0])
+                # print((bb[i][3]-bb[i][1])/frame.shape[0])
+                if (bb[i][3]-bb[i][1])/frame.shape[0]>0.25:
+                    cropped = frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :]
+                    scaled = cv2.resize(cropped, (INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE),
+                                        interpolation=cv2.INTER_CUBIC)
+                    scaled = facenet.prewhiten(scaled)
+                    scaled_reshape = scaled.reshape(-1, INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE, 3)
+                    feed_dict = {images_placeholder: scaled_reshape, phase_train_placeholder: False}
+                    emb_array = sess.run(embeddings, feed_dict=feed_dict)
+
+                    predictions = model.predict_proba(emb_array)
+                    best_class_indices = np.argmax(predictions, axis=1)
+                    best_class_probabilities = predictions[
+                        np.arange(len(best_class_indices)), best_class_indices]
+                    best_name = class_names[best_class_indices[0]]
+                    print("Name: {}, Probability: {}".format(best_name, best_class_probabilities))
+
+
+
+                    if best_class_probabilities > 0.8:
+                        cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)
+                        text_x = bb[i][0]
+                        text_y = bb[i][3] + 20
+
+                        name = class_names[best_class_indices[0]]
+                        cv2.putText(frame, name, (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                    1, (255, 255, 255), thickness=1, lineType=2)
+                        cv2.putText(frame, str(round(best_class_probabilities[0], 3)), (text_x, text_y + 17),
+                                    cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                    1, (255, 255, 255), thickness=1, lineType=2)
+                        person_detected[best_name] += 1
+                    else:
+                        name = "Unknown"
+                    
+                    return best_name, best_class_probabilities[0], frame
+        
+        return "Unknown", 0, frame
+    except:
+        return "Unknown", 0, frame
+        pass
+    
+
+def start():
+    global model, class_names, images_placeholder, embeddings, sess, pnet, rnet, onet, phase_train_placeholder
+    model, class_names = load_model()
+    images_placeholder,phase_train_placeholder, embeddings, sess, pnet, rnet, onet = load_facenet()
+
+def identify(frame):
+    return identify_face(frame, model, class_names, images_placeholder,phase_train_placeholder, embeddings, sess, pnet, rnet, onet)
+
